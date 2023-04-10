3bbea95df1b8630f033b91e55cec1e7a383899ae
Akido123 <117109568+Akido123@users.noreply.github.com>
2023-04-10 17:21:46 +0200

    Update; status: incomplete, errors: variable location will not be defined

--- Challenge1/script.js
+++ Challenge1/script.js
@@ -5,42 +5,33 @@
 // if a user’s cost without shipping is equal to or more than R1000 or $60 in cost and they are based in South Africa or Namibia, then their shipping should be free.
 // However, this is only the case if the number of customers is exactly 1, otherwise, it should show the message stored WARNING.
 
+let shipping = 0;
+let currency = '';
+const customers = 1;
+
 FREE_WARNING = 'Free shipping only applies to single customer orders'
 BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
 NONE_SELECTED = 0
 
-let location;
-let shipping; 
-let currency;
+if (location === 'RSA') {shipping = 400; currency = 'R'}
 
-if (location = "South Africa") { shipping === 400 && currency === 'R' }
-if (location = "Namibia") { shipping === 600 && currency === '$' }
-else { shipping === 800 && currency === '$' }
+if (location === 'NAM') {shipping = 600; currency = '$'}
 
-const shoes = 300 * 1
-const toys = 100 * 5
-const shirts = 150 * NONE_SELECTED
-const batteries = 35 * 2
-const pens = 5 * NONE_SELECTED 
+if (location === 'NK') {console.warn(BANNED_WARNIN)}
 
-shipping = null
-currency = $
+else {shipping = 800; currency = '$'}
 
-if (((shoes + batteries + pens + shirts > 1000 && currency === "R") || (shoes + batteries + pens + shirts > 60 && currency === "$"))) {
-	if (((location === "South Africa") || (location = "Namibia")) && customers < 2) {
-    shipping = 0
-	}
-  else{
-    console.warn(FREE_WARNING)
-  }
+shoes = 300 * 1
+toys = 100 * 5
+shirts = 150 * NONE_SELECTED
+batteries = 35 * 2
+pens = 5 * NONE_SELECTED 
+
+if (((shoes + batteries + pens + shirts + toys >= 1000 && currency === '$') && location === 'RSA') || (((shoes + batteries + pens + shirts + toys >= 1000 && currency === 'NAM') && location === 'RSA'))) {
+	if (customers === 1) {
+		    shipping = 0
+		}
 }
+else { console.log(FREE_WARNING) }
 
-if (shipping = 0 && customers !== 1) { 
-  console.log(WARNING) 
-}
-
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
-
-customers = '1'
-location = 'RSA'
-currency = null
+console.log('price', currency, shoes + batteries + pens + shirts + shipping)
