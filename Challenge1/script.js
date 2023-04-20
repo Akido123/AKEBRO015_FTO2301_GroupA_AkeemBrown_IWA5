9c6893512be08b81199a7957a17d7cd2cf57afdf
Akido123 <117109568+Akido123@users.noreply.github.com>
2023-04-20 19:51:21 +0200

    Update: completed and corrected challenge 1

--- Challenge1/script.js
+++ Challenge1/script.js
@@ -5,21 +5,27 @@
 // if a user’s cost without shipping is equal to or more than R1000 or $60 in cost and they are based in South Africa or Namibia, then their shipping should be free.
 // However, this is only the case if the number of customers is exactly 1, otherwise, it should show the message stored WARNING.
 
-let shipping = 0;
-let currency = '';
-const customers = 1;
+const local = 'RSA'
+const customers = 1
+let currency
+let shipping
 
 FREE_WARNING = 'Free shipping only applies to single customer orders'
 BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
 NONE_SELECTED = 0
 
-if (location === 'RSA') {shipping = 400; currency = 'R'}
-
-if (location === 'NAM') {shipping = 600; currency = '$'}
-
-if (location === 'NK') {console.warn(BANNED_WARNIN)}
-
-else {shipping = 800; currency = '$'}
+if (local === 'RSA') { 
+  shipping = 400
+  currency = 'R' 
+}
+else if(local === 'NAM') {
+  shipping = 600 
+  currency = '$'
+} 
+else {
+  shipping = 800
+  currency = '$'
+}
 
 shoes = 300 * 1
 toys = 100 * 5
@@ -27,11 +33,13 @@ shirts = 150 * NONE_SELECTED
 batteries = 35 * 2
 pens = 5 * NONE_SELECTED 
 
-if (((shoes + batteries + pens + shirts + toys >= 1000 && currency === '$') && location === 'RSA') || (((shoes + batteries + pens + shirts + toys >= 1000 && currency === 'NAM') && location === 'RSA'))) {
-	if (customers === 1) {
-		    shipping = 0
-		}
+if ((shoes + batteries + pens + shirts >= 1000 && currency === 'R') || (shoes + batteries + pens + shirts >= 60 && currency === '$')) {
+  if (local === 'NAM' || local === 'RSA') {
+    if (customers === 1){
+      shipping = 0
+    }
+    else{ console.log(FREE_WARNING) }
+	}
 }
-else { console.log(FREE_WARNING) }
 
-console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+local === 'NK' ? console.log(BANNED_WARNIN) : console.log('price', currency, shoes + batteries + pens + toys + shirts + shipping)
